c68347ac9ea20c182bd0239623d7e334e950019e
Aluno Fiar Logatti <aluno@fiar.local>
2018-03-02 20:15:36 -0300

    Implantação

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ano, mes, dia;
+
+            System.Console.WriteLine("Informe o ano de nascimento ");
+            ano = Console.ReadLine();
+            int anof =  Convert.ToInt32(ano);
+            int anofinal = (2017 - anof);
+
+            int diaAno = (anofinal * 365);
+            
+            System.Console.WriteLine("Informe o mes do seu nascimento");
+            mes = Console.ReadLine();
+            int mesfinal = Convert.ToInt32(mes);
+
+            int diaMes = (mesfinal * 30);
+
+            System.Console.WriteLine("Informe o dia do seu nascimento");
+            dia = Console.ReadLine();
+
+            int diaFinal = (diaAno + diaMes);
+
+
+            System.Console.WriteLine("Sua idade em dias eh : " + diaFinal);
+
+            Console.ReadKey();
+
+        }
+    }
+}
