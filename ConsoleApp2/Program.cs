0d3cac8c553e7f72cfd182e7a88b8f9c0ee48698
Aluno Fiar Logatti <aluno@fiar.local>
2018-03-02 21:07:26 -0300

    implantação Exercicio 1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,29 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Qual a quantidade de votos Brancos ?");
+            string votoBranco = Console.ReadLine();
+
+            Console.WriteLine("Qual a quantidade de votos Nulos ?");
+            string votosNulos = Console.ReadLine();
+
+            Console.WriteLine("Qual a quantidade de votos validos ");
+            string votosValidos = Console.ReadLine();
+
+            int  branco = Convert.ToInt32(votoBranco);
+            int  nulo = Convert.ToInt32(votosNulos);
+            int validos = Convert.ToInt32(votosValidos);
+
+            int total = (branco + nulo + validos);
+
+            Console.WriteLine("O total de votos eh: " +total);
+
+            Console.WriteLine("O percentual de votos branco eh :" + ((branco * 100) / total) + "%");
+            Console.WriteLine("O percentual de votos Nulos eh :" + ((nulo * 100) / total) + "%");
+            Console.WriteLine("O percentual de votos validos eh :" + ((validos * 100) / total) + "%");
+
+            Console.ReadKey();
+
         }
     }
 }
